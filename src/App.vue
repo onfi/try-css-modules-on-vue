1f6e9364a4efaea5f563bd2354a3cc2ca90393c7
shimoda.keitaro <shimoda.keitaro@raccoon.ne.jp>
2023-11-17 17:27:57 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// import AppCss from './assets/app.module.css'
+// import TestCss from './assets/test.module.css'
+</script>
+
+<template>
+  <header class="header">
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main class="main">
+    CSS modulesを使ってみよう！
+    <pre>
+    </pre>
+  </main>
+</template>
+
+<style>
+@import './assets/app.module.css';
+/* @import './assets/test.module.css'; */
+</style>
