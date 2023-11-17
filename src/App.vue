fab4bc39a155b0e0598777995cf07e956395ca98
shimoda.keitaro <shimoda.keitaro@raccoon.ne.jp>
2023-11-17 18:28:03 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script setup>
-import { header } from './assets/app.module.css'
-import TestCss from './assets/test.module.css'
+// import AppCss from './assets/app.module.css'
+// import TestCss from './assets/test.module.css'
 </script>
 
 <template>
-  <header :class="header">
+  <header class="header">
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
   </header>
 
-  <main :class="TestCss.main">
+  <main class="main">
     CSS modulesを使ってみよう！
     <pre>
     </pre>
   </main>
 </template>
+
+<style>
+/* @import './assets/app.module.css'; */
+/* @import './assets/test.module.css'; */
+</style>
